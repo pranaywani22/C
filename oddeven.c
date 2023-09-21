7c49d7315217fb685c90179cf96b87e947e7e4e4
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-21 11:19:18 +0530

    This is a C program sturctured to find weather the given number is Even or Odd

---
+++ oddeven.c
@@ -0,0 +1,12 @@
+/*
+Author :- Pranay Ajitkumar Wani
+*/
+void main(){
+    int i;
+    printf("\n Enter the number \t");
+    scanf("%d",&i);
+    if(i%2==0)
+    printf("\n %d is even",i);
+    else
+    printf("\n %d is odd",i);
+}
