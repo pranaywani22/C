a6639e1baab1ccade2df83a7d17cb9d9f74aa523
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-27 08:35:21 +0530

    Create demo.c

---
+++ demo.c
@@ -0,0 +1,11 @@
+/*
+Author:-Pranay Ajitkumar Wani
+the following code is structured in c to collect a int value store it and print it when required
+*/
+#include<stdio.h>
+void main(){
+  int x;
+  printf("\n Enter the value of X");
+  scanf("%d",&x);
+  printf("\n The value of X is %d",x)
+}
