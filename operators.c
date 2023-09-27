9c30e6db2d1d2714ac4d55f48ebd31e4b172984a
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-27 08:54:12 +0530

    Create operators.c

---
+++ operators.c
@@ -0,0 +1,35 @@
+/*
+Author:- Pranay Ajitkumar Wani
+This file is structured to show the use of matheatical operators in c
+*/
+#include<stdio.h>
+void main(){
+  int a,b,c,d,e,f,g,h,i,j;
+  printf("\n Enter 2 int values to add");
+  scnaf("%d%d",&a,&b);
+  printf("given values are %d %d",a,b);
+  printf("Addition of the two are %d",a+b);
+  
+  printf("\n Enter 2 int values to substract");
+  scnaf("%d%d",&ac,&d);
+  printf("given values are %d %d",c,d);
+  printf("Addition of the two are %d",c-d);
+  
+  printf("\n Enter 2 int values to multiply");
+  scnaf("%d%d",&e,&f);
+  printf("given values are %d %d",e,f);
+  printf("Addition of the two are %d",e*f);
+  
+  printf("\n Enter 2 int values to divide");
+  scnaf("%d%d",&g,&h);
+  printf("given values are %d %d",g,h);
+  printf("Addition of the two are %d",g/h);
+
+  printf("\n Enter 2 int values to remainder");
+  scnaf("%d%d",&i,&j);
+  printf("given values are %d %d",i,j);
+  printf("Addition of the two are %d",i%j);
+  
+  
+  
+}
