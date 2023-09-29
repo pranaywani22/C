2b90421821392bf978de0f8d9f45e3b014f1bb56
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-09-29 09:19:29 +0530

    Add files via upload

---
+++ assignment.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int x,y,z;
+printf("\n Enter the value of A\t");
+scanf("%d",&x);
+printf("\n Enter the value of B\t");
+scanf("%d",&y);
+z=y;
+printf("\n The Value of X=%d \t Y=%d \t Z=%d",x,y,z);
+
+}
