f3af5280220f80cfa0770ea2d776c3cea82a8bbd
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-10-01 19:25:31 +0530

    Add files via upload

---
+++ logicalorr.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int r,x,y,z;
+printf("\n Enter the value of X\t");
+scanf("%d",&x);
+printf("\n Enter the value of Y\t");
+scanf("%d",&y);
+z=x+y-3;
+printf("\n Enter the value of X=%d \t Y=%d \t Z=%d ",x,y,z);
+r=x<y || x<z;
+printf("\n X<Y || X<Z =%d",r);
+r=x<y || x>z;
+printf("\n X<Y || X>Z =%d",r);
+r=x>y || x>z;
+printf("\n X>Y || X>Z =%d",r);
+r=x>y || x<z;
+printf("\n X>Y || X<Z =%d",r);
+}
+
