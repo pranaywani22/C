40c2397eb2e616f25931c49242700a76a879a15c
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-10-02 10:22:13 +0530

    Add files via upload

---
+++ postincrdrec.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int x,y;
+printf("\n Enter the vlaue of X:\t");
+scanf("%d",&x);
+printf("\n Enter the Value of Y:\t");
+scnaf("%d",&y);
+x=x+1;
+printf("\n Value of X+1=%d ",x);
+x++;
+printf("\n Value of X++ %d",x);
+y=y+1;
+printf("\n Value of Y+1=%d ",y);
+y++;
+printf("\n Value of Y++ %d",y);
+}
+
