099274173da117d65503f7c357066b53f2924de5
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-10-03 13:53:26 +0530

    Add files via upload

---
+++ nestedternary.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main (){
+int x,y,z,r;
+printf("\n Enter the three number ");
+scanf("%d%d%d",&x,&y,&z);
+printf("\n Value of X=%d \t Y=%d \t Z=%d",x,y,z);
+r=x>y?(x>z?x:z):(y>z?y:z);
+printf("\n Max number =%d",r);
+}
