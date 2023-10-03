d8da2de1b7b09a04078e4b363afbac7d608084fa
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-10-03 13:54:05 +0530

    Add files via upload

---
+++ castiing.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int a,b,c;
+    float x,y,z;
+    a=45;
+    b=23;
+    x=15.25;
+    y=78.36;
+    c=a+b;
+printf("\n %d+%d =%d",a,b,c);
+z=x+y;
+printf("\n %f+%f=%f",x,y,z);
+z=x+y;
+printf("\n %f+%d=%f",x,y,z);
+}
