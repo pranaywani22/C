df11e6513ec5bb81f8c5638606b7c86d299d643e
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-10-04 17:18:39 +0530

    Add files via upload

---
+++ salary.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main(){
+long int sal,hra=1000,ta=1200,da=1000;
+printf("\n Enter the Employee salary");
+scanf("%ld",&sal);
+printf("")
+}
