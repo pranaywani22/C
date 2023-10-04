6b2a1c86a51e42a219db19431fb0512e5f563af3
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-10-04 17:24:29 +0530

    Update decisionif.c

--- decisionif.c
+++ decisionif.c
@@ -1,1 +1,17 @@
-
+/*
+Author:- Pranay Ajitkumar Wani
+This coode is strutuurred in C programming to practice decision controls using if 
+*/
+#include<stdio.h>
+#include<conio.h>
+int main(){
+  int a,b,c;
+  printf("\n Enteer your salary");
+  scanf("%d",&a);
+  if(a>5000)
+  {
+    printf("\n you get a bonous");
+  }
+  else
+    printf("\n You ont get bonous");
+}
