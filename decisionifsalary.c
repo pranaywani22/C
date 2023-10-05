dd5f3b3c219c60c9a473847dadbc44b09fc9c433
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-10-05 21:30:25 +0530

    Add files via upload

---
+++ decisionifsalary.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main(){
+float hr,ta,da,bs,gs;
+printf("\n Enter your basic salary");
+scanf("%f",&bs);
+{ta= (0.05)*bs;
+hr= (0.25)*bs;
+da= (0.07)*bs;
+gs=bs+hr+ta+da;}
+printf("\n hr=%f \t ta=%f \t da=%f",hr,ta,da);
+printf("\n Your gross salary is %f",gs);
+if(gs>35000)
+{
+printf("\n yo get a bonous of 2500INR");
+gs=gs+2500;
+printf("\n your Gross salary of thee month is %f",gs);
+}
+else
+printf("\n you are not eligible for the bonous");
+}
+
