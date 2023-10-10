9c5314f502963a3a517ce8fd3aec6aadfc1b7224
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-10-10 08:29:13 +0530

    Add files via upload

---
+++ sumn.c
@@ -0,0 +1,13 @@
+/*This code is designed in c programming to add the number asked by the user using while loop*/
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    int i=0,no,sum=0;
+    printf("\n Enter the number till which you want to add");
+    scanf("%d",&no);
+    while(i<=no){
+        sum=sum+i;
+        printf("\n %d",sum);
+        i++;
+    }
+}
