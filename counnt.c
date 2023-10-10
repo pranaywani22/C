512e9925659ec428a4c55efd99e4e22c5d1f01b1
pranaywani22 <145085703+pranaywani22@users.noreply.github.com>
2023-10-10 08:28:53 +0530

    Add files via upload

---
+++ counnt.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int i=0,no;
+printf("\n How long you want to make the loop run");
+scanf("%d",&no);
+while(i<=no){
+printf("\n Value of i= %d",i);
+i++;
+}
+}
+
