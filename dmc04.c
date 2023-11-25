fbe82703c7607121ade37a920b4ddfdd6ffe25c7
Pranay_Wani <145085703+pranaywani22@users.noreply.github.com>
2023-11-25 14:32:41 +0530

    dyanamic memory allocation

---
+++ dmc04.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+    float *ptr;
+    int n,i;
+    printf("\n Enter the number of elements u want the storage for:- ");
+    scanf("%d",&n);
+    ptr= (float*)malloc(n*sizeof(float));
+    for(i=0;i<n;i++){
+        printf("\n Enter %d float element ",i+1);
+        scanf("%f",&ptr[i]);
+    }
+    for(i=0;i<n;i++){
+        printf("\n Elemnt entered on %d is %f",i+1,ptr[i]);
+    }
+    free(ptr);
+    return 0;
+}
